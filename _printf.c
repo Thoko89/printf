a4a15560323cd579d34027b28ee6215ba02f0814
Thoko89 <thokozanilomntu58@gmail.com>
2023-07-24 03:52:42 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,78 +1,52 @@
 #include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 #include <unistd.h>
-#include "holberton.h"
-/**
-  * find_function - function that finds formats for _printf
-  * calls the corresponding function.
-  * @format: format (char, string, int, decimal)
-  * Return: NULL or function associated ;
-  */
-int (*find_function(const char *format))(va_list)
-{
-	unsigned int i = 0;
-	code_f find_f[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"i", print_int},
-		{"d", print_dec},
-		{"r", print_rev},
-		{"b", print_bin},
-		{"u", print_unsig},
-		{"o", print_octal},
-		{"x", print_x},
-		{"X", print_X},
-		{"R", print_rot13},
-		{NULL, NULL}
-	};
+#include "main.h"
 
-	while (find_f[i].sc)
-	{
-		if (find_f[i].sc[0] == (*format))
-			return (find_f[i].f);
-		i++;
-	}
-	return (NULL);
-}
 /**
-  * _printf - function that produces output according to a format.
-  * @format: format (char, string, int, decimal)
-  * Return: size the output text;
-  */
+ * _printf - produces output according to a format
+ * @format: The specified format
+ *
+ * Return: The number of characters that were printed
+ */
 int _printf(const char *format, ...)
 {
-	va_list ap;
-	int (*f)(va_list);
-	unsigned int i = 0, cprint = 0;
+	int  i = 0, k = 0;
+	int n_displayed = 0;
+	char *str = NULL;
+	va_list args;
+	int (*func)(va_list);
 
-	if (format == NULL)
-		return (-1);
-	va_start(ap, format);
-	while (format[i])
+	va_start(args, format);
+
+	while (format[i] != '\')
 	{
-		while (format[i] != '%' && format[i])
+		if (format[i] != '%')
 		{
 			_putchar(format[i]);
-			cprint++;
-			i++;
+			n_displayed++;
 		}
-		if (format[i] == '\0')
-			return (cprint);
-		f = find_function(&format[i + 1]);
-		if (f != NULL)
+		else if (format[i + 1] == '%')
 		{
-			cprint += f(ap);
-			i += 2;
-			continue;
-		}
-		if (!format[i + 1])
-			return (-1);
-		_putchar(format[i]);
-		cprint++;
-		if (format[i + 1] == '%')
-			i += 2;
-		else
 			i++;
+			_putchar('%');
+			n_displayed++;
+		}
+		else
+		{
+			func = _selec_func(format[i + 1]);
+			if (func != NULL)
+			{
+				n_displayed += func(args);
+				i++;
+			}
+		}
+
+		i++;
 	}
-	va_end(ap);
-	return (cprint);
+
+	va_end(args);
+	return (n_displayed);
 }
